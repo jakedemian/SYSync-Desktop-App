6d7cbdde34b6e31e2b95fabd937edc34f9bc83d4
jakedemianrosetta <Jacob.Demian@rosetta.com>
2016-12-14 22:23:59 -0500

    more stuff implemented both in styling and javascript.

--- app.js
+++ app.js
@@ -3,8 +3,12 @@ window.app = {
 
 	conversationData: [],
 	currentConversationNumber: null,
+	myNumber: "4408971768",
 
 	obtainAllConversationData : function(){
+		/*
+				TODO replace this a real call to the webservice
+		*/
 		this.conversationData = {
 			"4405919000" : {
 				"name" : "Alison Delaney",
@@ -22,7 +26,7 @@ window.app = {
 					},
 					{
 						"src" : "4408971768",
-						"msg" : "what's up",
+						"msg" : "what's up this is a really long message because I'm testing what happens when i have an incredibly long message just like this one, which as I said is very very very long.",
 						"timestamp" : "2016-12-14-4:45:01"
 					},
 					{
@@ -60,7 +64,7 @@ window.app = {
 				if(this.currentConversationNumber != phoneNumber){
 					// since this is an onclick event, I need to reference my globals as app.x, not this.x
 					app.currentConversationNumber = phoneNumber;
-					app.loadConversation(phoneNumber);
+					app.loadConversation(app.conversationData[phoneNumber].msgs);
 				}
 			});
 			
@@ -70,10 +74,28 @@ window.app = {
 
 			$(".contact-list").append(contactCard);
 		}
+
+		if(this.currentConversationNumber == null){
+			// automatically load the first conversation in the contact-list
+			var firstContactInList = $(".contact-list").children().first();
+			if(firstContactInList.length > 0){
+				var number = firstContactInList.data("number");
+				this.loadConversation(this.conversationData[number].msgs);
+			}
+		}
 	},
 
-	loadConversation : function(number){
-		console.log("TODO: loading the conversation from the stored conversationData json object!");
+	loadConversation : function(messages){
+		for(var i = 0; i < messages.length; i++){
+			var msg = messages[i];
+			var myNum = app.myNumber;
+
+			var msgDiv = $("<div>");
+			msgDiv.addClass("msg");
+			myNum == msg.src ? msgDiv.addClass("msg-me") : msgDiv.addClass("msg-them");
+			msgDiv.html(msg.msg);
+			$(".conversation-area").append(msgDiv);
+		}
 	}
 };
 }
